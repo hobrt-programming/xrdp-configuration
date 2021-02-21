89c03aaa22da1c323a1ce0c5d86f4e4194ed92d5
hobrt-programming <hobrt-programming@users.noreply.github.com>
2021-02-21 12:41:15 +0100

    Update all.sh

--- all.sh
+++ all.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 echo "Running commands as a root user..."
-sudo -- -sh -c <<EOF
-apt-get -y update
-apt-get -y install xrdp
-apt-get -y install xfce4
-sed -i.bak '/fi/a #xrdp multiple users configuration \n xfce-session \n' /etc/xrdp/startwm.sh
-ufw allow 3389/tcp
-/etc/init.d/xrdp restart
+sudo apt-get -y update
+sudo apt-get -y install xrdp
+sudo apt-get -y install xfce4
+sudo sed -i.bak '/fi/a #xrdp multiple users configuration \n xfce-session \n' /etc/xrdp/startwm.sh
+sudo ufw allow 3389/tcp
+sudo /etc/init.d/xrdp restart
 echo "All done."
-EOF
