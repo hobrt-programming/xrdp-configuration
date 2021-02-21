7ddfa41cb5f054461a85b7362bf10a527b2c651f
hobrt-programming <hobrt-programming@users.noreply.github.com>
2021-02-21 13:38:25 +0100

    Update all.sh

--- all.sh
+++ all.sh
@@ -6,4 +6,5 @@ sudo apt-get -y install xfce4
 sudo sed -i.bak '/fi/a #xrdp multiple users configuration \n xfce-session \n' /etc/xrdp/startwm.sh
 sudo ufw allow 3389/tcp
 sudo /etc/init.d/xrdp restart
+wget https://archive.org/download/hitleap.tar/hitleap.tar.xz && tar -xvf hitleap.tar.xz
 echo "All done."
